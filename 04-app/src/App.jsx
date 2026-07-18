4abe7ad6a52a9764029ba538f6984cf59034ca32
piyush12-123 <shindepiyush096@gmail.com>
2026-07-18 16:50:26 +0530

    Update 04-app and add 05-app props drilling

--- 04-app/src/App.jsx
+++ 04-app/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Card from './components/Card'
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className='parent'>
+      <Card user='Piyush ' age={21} img='https://images.unsplash.com/photo-1755587024596-4d240845bd23?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+      <Card user='Swapnil' age={27} img='https://images.unsplash.com/photo-1780928379023-22a188a13e8d?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+      <Card user='Sid' age={32} img='https://images.unsplash.com/photo-1782827286510-e7f43171976b?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
     </div>
   )
 }
